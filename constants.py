877a59fea2761c4458224148ac8c8ff20744af06
Florimond Manca <florimond.manca@gmail.com>
2016-08-12 11:59:46 +0200

    Tiles en n 32x32

--- constants.py
+++ constants.py
@@ -22,7 +22,7 @@ TILE_CATEGORIES = list(filter(lambda f: not f.startswith("."), os.listdir(os.pat
 FPS = 30
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-TILE_SIZE = 50  # pixels
+TILE_SIZE = 32  # pixels
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # set a screen now (needed for the TilesLibrary)
 
 CASE_SIZE = 120
